3c0f7d71ddd075d6e09c6bdbd91ed10a9ebcdf26
Ilam Ball <informasiilam@gmail.com>
2024-03-26 14:32:59 +0700

    prosedur inputData

---
+++ fungsiswitch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+void inputData(){
+    cout << "Masukkan Bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "Masukkan Bilangan Kedua : ";
+    cin >> bilangan2;
+}
+
+int main(){
+
+}
