f6c69b958eaa5e2e5b3e452213ed311a25d52f83
Ilam Ball <informasiilam@gmail.com>
2024-03-26 15:33:04 +0700

    perbaikan system pause dan clear

--- fungsiswitch.cpp
+++ fungsiswitch.cpp
@@ -3,30 +3,35 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
-void inputData(){
+void inputData()
+{
     cout << "Masukkan Bilangan pertama : ";
     cin >> bilangan1;
     cout << "Masukkan Bilangan Kedua : ";
     cin >> bilangan2;
 }
 
-
-int penjumlahan(int a, int b){
+int penjumlahan(int a, int b)
+{
     return a + b;
 }
-int pengurangan(int a, int b){
+int pengurangan(int a, int b)
+{
     return a - b;
 }
-int perkalian(int a, int b){
+int perkalian(int a, int b)
+{
     return a * b;
 }
-float pembagian(int a, int b){
+float pembagian(int a, int b)
+{
     return a / b;
 }
 
 int main()
 {
     int pilihan;
+
     do
     {
         cout << "Kalkulator Sederhana" << endl;
@@ -42,33 +47,30 @@ int main()
 
         switch (pilihan)
         {
-            case 1:
-                inputData();
-                cout << "Hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
-                break;
-            case 2:
-                inputData();
-                cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
-                break;
-            case 3:
-                inputData();
-                cout << "Hasil perkalian = " << perkalian(bilangan1, bilangan2) << endl;
-                break;
-            case 4:
-                inputData();
-                cout << "Hasil pembagian = " << pembagian(bilangan1, bilangan2) << endl;
-                break;
-            case 5:
-                break;
-            
-            default:
-                cout << "Pilihan Tidak Ada" << endl;
-                break;
+        case 1:
+            inputData();
+            cout << "Hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
+            break;
+        case 2:
+            inputData();
+            cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
+            break;
+        case 3:
+            inputData();
+            cout << "Hasil perkalian = " << perkalian(bilangan1, bilangan2) << endl;
+            break;
+        case 4:
+            inputData();
+            cout << "Hasil pembagian = " << pembagian(bilangan1, bilangan2) << endl;
+            break;
+        case 5:
+            break;
+
+        default:
+            cout << "Pilihan Tidak Ada" << endl;
+            break;
         }
-    } while (pilihan !=5);
-
-    system("pause");
-    system("CLS");
-
-    
+        system("pause");
+        system("CLS");
+    } while (pilihan != 5);
 }
